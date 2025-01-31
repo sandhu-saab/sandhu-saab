6ce6d5748e49fc0d36afa45dd3b2fae8acbcb5a4
sandhu-saab <sandhusaab61291@gmail.com>
2025-01-31 14:24:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('AUTH_CHANNEL', '-10
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sandhusaab61291:Sandhu@cluster0.c67tt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://sandhusaab050505:Sandhu2@cluster0.lfrnj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "sandhusaab61291")
+DATABASE_NAME = environ.get('DATABASE_NAME', "sandhusaab61291 sandhusaab050505")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
 # ============================
